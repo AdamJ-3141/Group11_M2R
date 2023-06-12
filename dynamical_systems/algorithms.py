72a0f1784362fe2b83542aa6a9c19139289cfe60
Adam Johnson <arjohnson14@outlook.com>
2023-06-12 13:15:37 +0100

    Added regularization parameter

--- dynamical_systems/algorithms.py
+++ dynamical_systems/algorithms.py
@@ -29,7 +29,7 @@ def find_approximation(system: callable, t1: float,
     U_i = U[:, :-1]
     W_in = w * (2 * np.random.random((D_r, D)) - 1)
     b_in = b * (2 * np.random.random((D_r, 1)) - 1)
-    Phi = np.tanh(W_in @ U_i + b_in)
+    Phi = (1/np.sqrt(D_r))*np.tanh(W_in @ U_i + b_in)
     W_LR = (U_o @ Phi.T @ inv(Phi @ Phi.T + beta * np.identity(D_r)))
 
     return U, W_LR, W_in, b_in, dt
@@ -37,10 +37,11 @@ def find_approximation(system: callable, t1: float,
 
 def propagate_from_u0(U, W_LR, W_in, b_in):
     N = U.shape[1] - 1
+    D_r = b_in.shape[0]
     U_hat = np.atleast_2d(U[:, 0]).T
     for n in range(N):
         u_hat_n = U_hat[:, -1]
-        phi = np.tanh(np.atleast_2d(W_in @ u_hat_n).T + b_in)
+        phi = (1/np.sqrt(D_r))*np.tanh(np.atleast_2d(W_in @ u_hat_n).T + b_in)
         u_np1 = W_LR @ phi
         U_hat = np.concatenate((U_hat, u_np1), axis=1)
     return U_hat
@@ -49,15 +50,16 @@ def propagate_from_u0(U, W_LR, W_in, b_in):
 def propagate_until_diverge(U, t1, W_LR, W_in, b_in):
     N = U.shape[1] - 1
     dt = t1 / (N + 1)
+    D_r = b_in.shape[0]
     U_hat = np.atleast_2d(U[:, 0]).T
     tau_f = t1
     for n in range(N):
         u_hat_n = U_hat[:, -1]
         u_n = U[:, n]
-        phi = np.tanh(np.atleast_2d(W_in @ u_hat_n).T + b_in)
+        phi = (1/np.sqrt(D_r))*np.tanh(np.atleast_2d(W_in @ u_hat_n).T + b_in)
         u_np1 = W_LR @ phi
         rel_error = np.linalg.norm(u_hat_n - u_n)/np.linalg.norm(u_n)
-        if rel_error > 0.005:
+        if rel_error > 0.05:
             tau_f = n * dt
             return tau_f
         U_hat = np.concatenate((U_hat, u_np1), axis=1)
